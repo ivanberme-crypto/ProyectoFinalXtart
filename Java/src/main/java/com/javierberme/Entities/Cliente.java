0164a98ce4ced8ba73920bc87e14b5c870bde0b6
berme_ivan <bermevfx@gmail.com>
2026-05-12 09:20:00 +0200

    Add Cliente entity

---
+++ Java/src/main/java/com/javierberme/Entities/Cliente.java
@@ -0,0 +1,73 @@
+package com.javierberme.Entities;
+
+public class Cliente {
+
+    protected int idCliente;
+    protected String nombre;
+    protected String email;
+    private String telefono;
+    private String direccion;
+
+    public Cliente() {
+    }
+
+    public Cliente(int idCliente, String nombre, String email,
+                   String telefono, String direccion) {
+        this.idCliente = idCliente;
+        this.nombre = nombre;
+        this.email = email;
+        this.telefono = telefono;
+        this.direccion = direccion;
+    }
+
+    public int getIdCliente() {
+        return idCliente;
+    }
+
+    public void setIdCliente(int idCliente) {
+        this.idCliente = idCliente;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    @Override
+    public String toString() {
+        return "Cliente{" +
+                "idCliente=" + idCliente +
+                ", nombre='" + nombre + '\'' +
+                ", email='" + email + '\'' +
+                ", telefono='" + telefono + '\'' +
+                ", direccion='" + direccion + '\'' +
+                '}';
+    }
+}
